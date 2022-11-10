5ea1f9cb6812d4704dbdbdbdb17b7dee2afb8d28
krupadav <107058808+krupadav@users.noreply.github.com>
2022-11-10 22:14:15 +0530

    Add files via upload

---
+++ one_liner.py
@@ -0,0 +1,10 @@
+# Swap numbers
+a = int(input('Enter a number:'))
+b = int(input('Enter second number:'))
+a, b = b, a
+print(a, b)
+# Reverse a string:
+s = "My string"
+print(s[::-1])
+# string palindrome:
+s = s[::-1]
